64298bc314acc84d1afa200746dbb4b183a51afa
jimin song <songing01@gmail.com>
2024-07-18 01:09:49 +0900

    💄 NavBar 퍼블리싱

---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+import map_grey from "../../assets/icons/map_grey.svg";
+import profile_grey from "../../assets/icons/profile_grey.svg";
+import plog_grey from "../../assets/icons/plog_grey.svg";
+
+const NavBar = () => {
+  return (
+    <Wrapper>
+      <img src={map_grey} />
+      <img src={plog_grey} />
+      <img src={profile_grey} />
+    </Wrapper>
+  );
+};
+
+export default NavBar;
+
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+
+  display: flex;
+  box-sizing: border-box;
+  width: 100%;
+  height: 57px;
+  padding: 3px 20px;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+`;
