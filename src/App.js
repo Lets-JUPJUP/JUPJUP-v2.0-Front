bd49c88c4f3ccbacb25d7cb762e11b7456fba788
jimin song <songing01@gmail.com>
2024-07-17 16:35:28 +0900

    🎉 프로젝트 기본 셋팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/auth/LoginPage";
+import LoginLoadingPage from "./pages/auth/LoginLoadingPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login/loading" element={<LoginLoadingPage />} />
+      </Routes>
     </div>
   );
 }
