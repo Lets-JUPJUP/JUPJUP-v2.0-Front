e2e819123612f1af62aa09099ea06f940511d9ad
jimin song <songing01@gmail.com>
2024-07-18 16:45:09 +0900

    💄 반응형 가로 길이 수정

--- src/components/common/LongBtn.jsx
+++ src/components/common/LongBtn.jsx
@@ -3,16 +3,22 @@ import styled from "styled-components";
 
 const LongBtn = ({ isActive = false, text }) => {
   return (
-    <>
+    <Wrapper>
       <Btn>{text}</Btn>
-    </>
+    </Wrapper>
   );
 };
 
 export default LongBtn;
 
+const Wrapper = styled.div`
+  width: 100vw;
+  box-sizing: border-box;
+  padding: 0 20px;
+`;
+
 const Btn = styled.div`
-  width: 350px;
+  width: 100%;
   display: flex;
   box-sizing: border-box;
   padding: 12px 10px;
