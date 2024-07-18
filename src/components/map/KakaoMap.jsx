961fa42e754b2ec53d2e781c404079a905e886e8
jimin song <songing01@gmail.com>
2024-07-18 15:31:40 +0900

    ✨ 지도 드래그로 중심 재설정 구현

--- src/components/map/KakaoMap.jsx
+++ src/components/map/KakaoMap.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
 import SearchBar from "./SearchBar";
+import gps from "../../assets/icons/gps.svg";
 
 const { kakao } = window;
 
@@ -8,6 +9,8 @@ const KakaoMap = () => {
   const [keyword, setKeyword] = useState("");
   const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
+
+  const [refresh, setRefresh] = useState(false);
   const mapRef = useRef(null);
 
   useEffect(() => {
@@ -45,7 +48,7 @@ const KakaoMap = () => {
     } else {
       console.log("Geolocation을 사용할 수 없습니다."); // Geolocation 미지원 로그
     }
-  }, []);
+  }, [refresh]);
 
   useEffect(() => {
     if (lat !== 0 && lon !== 0) {
@@ -55,6 +58,24 @@ const KakaoMap = () => {
       }
     }
   }, [lat, lon]);
+
+  useEffect(() => {
+    // 마우스 드래그로 지도 이동이 완료되었을 때 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(mapRef.current, "dragend", function () {
+      // 지도 중심 좌표를 얻어옵니다
+      var latlng = mapRef.current.getCenter();
+
+      var message = "변경된 지도 중심좌표는 " + latlng.getLat() + " 이고, ";
+      message += "경도는 " + latlng.getLng() + " 입니다";
+
+      console.log(message);
+    });
+  }, []);
+
+  const refreshMap = () => {
+    setRefresh(!refresh);
+    setKeyword("");
+  };
 
   return (
     <Wrapper>
@@ -66,6 +87,11 @@ const KakaoMap = () => {
           setLon={setLon}
         />
       </div>
+      <div className="btns">
+        <div className="btn">현 지도로 재검색</div>
+        <img className="gps" src={gps} onClick={refreshMap} />
+      </div>
+
       <Map id="map"></Map>
     </Wrapper>
   );
@@ -83,6 +109,36 @@ const Wrapper = styled.div`
     top: 16px;
     left: 0;
     z-index: 10;
+  }
+
+  .btns {
+    width: 100%;
+    position: absolute;
+    top: 68px;
+    box-sizing: border-box;
+    padding: 0 20px;
+    z-index: 10;
+    display: flex;
+    justify-content: end;
+
+    .gps {
+    }
+
+    .btn {
+      position: absolute;
+      right: 50%;
+      transform: translate(50%, 0%);
+      box-sizing: border-box;
+      display: inline-flex;
+      padding: 8px 10px;
+      justify-content: center;
+      align-items: center;
+      border-radius: 4px;
+      background: var(--main);
+
+      color: var(--white);
+      font-weight: 600;
+    }
   }
 `;
 
