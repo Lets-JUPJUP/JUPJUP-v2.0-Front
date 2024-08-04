a24c36be916bef17857c8d908ec87a9715ecc615
jimin song <songing01@gmail.com>
2024-08-05 00:08:28 +0900

    ✨ RecoilRoot 추가

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./reset.css";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </BrowserRouter>
 );
