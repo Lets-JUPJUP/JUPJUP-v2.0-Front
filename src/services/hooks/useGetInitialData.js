7826c4347521cbfb336b15209004084dc0d54ac1
jimin song <songing01@gmail.com>
2024-08-06 21:21:48 +0900

    ♻️ param 추가

--- src/services/hooks/useGetInitialData.js
+++ src/services/hooks/useGetInitialData.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const useGetInitialData = (requestFunc, headers, body) => {
+const useGetInitialData = (requestFunc, param, headers, body) => {
   const [data, setData] = useState(); // api 데이터 저장하기
   const [loading, setLoading] = useState(false); // 데이터 로딩 상태
   const [error, setError] = useState(false); // 에러 발생 상태
@@ -11,7 +11,7 @@ const useGetInitialData = (requestFunc, headers, body) => {
       setLoading(true);
       try {
         // 데이터 가져오기
-        const response = await requestFunc(headers, body);
+        const response = await requestFunc(param, headers, body);
         setData(response.data.data);
       } catch (err) {
         setError(err);
