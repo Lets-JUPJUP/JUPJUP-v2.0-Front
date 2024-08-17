885c7b8c697b33040196521961fc98a1558e2209
jimin song <songing01@gmail.com>
2024-08-17 17:36:47 +0900

    ✨ 마이페이지 api 연결

--- src/components/user/mypage/New.jsx
+++ src/components/user/mypage/New.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import right from "../../../assets/mypage/right.svg";
 import Item from "../../list/Item";
+import useGetInitialData from "../../../services/hooks/useGetInitialData";
+import { postGetCompletePost } from "../../../services/api/post";
 
 const New = () => {
-  return (
-    <Wrapper>
-      <div className="title">최근 완료한 플로깅</div>
-      <>
-        <div className="divider" />
-        <Item />
-        <div className="divider" />
-      </>
+  const { data } = useGetInitialData(postGetCompletePost);
 
-      <div className="review-btn">
-        다른 플로깅 리뷰하기
-        <img src={right} />
-      </div>
-    </Wrapper>
+  return (
+    data && (
+      <Wrapper>
+        <div className="title">최근 완료한 플로깅</div>
+        <>
+          <div className="divider" />
+          <Item item={data} />
+          <div className="divider" />
+        </>
+
+        <div className="review-btn">
+          다른 플로깅 리뷰하기
+          <img src={right} />
+        </div>
+      </Wrapper>
+    )
   );
 };
 
--- src/services/api/post.js
+++ src/services/api/post.js
@@ -48,3 +48,32 @@ export const postCreateBookmark = async (id) => {
 export const postDeleteBookmark = async (id) => {
   return await client.delete(`/api/v1/hearts/${id}`);
 };
+//가장 최근 완료한 플로깅 리스트
+export const postGetCompletePost = async () => {
+  return await client.get(`/api/v1/posts/latest-completed`);
+};
+
+//[완료]한 플로깅 리스트
+export const postGetCompleteList = async () => {
+  return await client.get(`/api/v1/posts/completed`);
+};
+
+//[모집완료]한 플로깅 리스트
+export const postGetSuccessList = async () => {
+  return await client.get(`/api/v1/posts/successful`);
+};
+
+//[모집중]인 플로깅 리스트
+export const postGetRecruitingList = async () => {
+  return await client.get(`/api/v1/posts/recruiting`);
+};
+
+//[전체] 플로깅 리스트
+export const postGetWholeList = async () => {
+  return await client.get(`/api/v1/posts/list`);
+};
+
+//북마크한 플로깅 리스트
+export const postGetBookmarkList = async () => {
+  return await client.get(`/api/v1/hearts/lists`);
+};
