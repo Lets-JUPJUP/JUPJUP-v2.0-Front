485f19b28e35fe19acd8a283b9a7616fbba519e4
jimin song <songing01@gmail.com>
2024-08-18 03:18:34 +0900

    ✨ 네브바 페이지이동, 색상변경

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import map_grey from "../../assets/icons/map_grey.svg";
+import map_color from "../../assets/icons/map_color.svg";
 import profile_grey from "../../assets/icons/profile_grey.svg";
+import profile_color from "../../assets/icons/profile_color.svg";
 import plog_grey from "../../assets/icons/plog_grey.svg";
+import plog_color from "../../assets/icons/plog_color.svg";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = ({ isNoGap = false }) => {
+  const navigate = useNavigate();
+  const [selected, setSelected] = useState(0);
+  const currentUrl = window.location.pathname;
+
+  useEffect(() => {
+    if (currentUrl.includes("map")) {
+      setSelected(1);
+    } else if (currentUrl.includes("list")) {
+      setSelected(2);
+    } else if (currentUrl.includes("mypage")) {
+      setSelected(3);
+    }
+  }, [currentUrl]);
+
   return (
     <Gap $isNoGap={isNoGap}>
       <Wrapper>
-        <img src={map_grey} />
-        <img src={plog_grey} />
-        <img src={profile_grey} />
+        {selected == 1 ? (
+          <img src={map_color} />
+        ) : (
+          <img src={map_grey} onClick={() => navigate("/map")} />
+        )}
+        {selected == 2 ? (
+          <img src={plog_color} />
+        ) : (
+          <img src={plog_grey} onClick={() => navigate("/list")} />
+        )}
+        {selected == 3 ? (
+          <img src={profile_color} />
+        ) : (
+          <img src={profile_grey} onClick={() => navigate("/mypage")} />
+        )}
       </Wrapper>
     </Gap>
   );
